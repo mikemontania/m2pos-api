c1647d24f029ff8e90f6badb2c88eb097614d6d9
miguel montania <mikemontania@gmail.com>
2024-01-13 18:58:28 -0300

    buscador de ventas

--- src/controllers/venta-controller.js
+++ src/controllers/venta-controller.js
@@ -1,23 +1,23 @@
-const { Op } = require('sequelize');
-const Venta = require('../models/venta.model'); // Asegúrate de que la importación del modelo sea correcta
-const VentaDetalle = require('../models/ventaDetalle.model');
-const { sequelize } = require('../../dbconfig');
- const moment = require('moment');
-const Numeracion = require('../models/numeracion.model');
-const Cliente = require('../models/cliente.model');
-const Sucursal = require('../models/sucursal.model');
-const FormaVenta = require('../models/formaVenta.model');
-const Usuario = require('../models/usuario.model');
-const ListaPrecio = require('../models/listaPrecio.model');
+const { Op } = require("sequelize");
+const Venta = require("../models/venta.model"); // Asegúrate de que la importación del modelo sea correcta
+const VentaDetalle = require("../models/ventaDetalle.model");
+const { sequelize } = require("../../dbconfig");
+const moment = require("moment");
+const Numeracion = require("../models/numeracion.model");
+const Cliente = require("../models/cliente.model");
+const Sucursal = require("../models/sucursal.model");
+const FormaVenta = require("../models/formaVenta.model");
+const Usuario = require("../models/usuario.model");
+const ListaPrecio = require("../models/listaPrecio.model");
 const getById = async (req, res) => {
   const { id } = req.params;
 
   const venta = await Venta.findByPk(id);
 
-  const detallesVenta = await VentaDetalle.findAll({where: { ventaId: id }  });
+  const detallesVenta = await VentaDetalle.findAll({ where: { ventaId: id } });
   res.status(200).json({
     detallesVenta,
-    venta,
+    venta
   });
 };
 // Crear una venta con sus detalles
@@ -54,42 +54,49 @@ const createVenta = async (req, res) => {
     }
 
     // Generar número de factura
-    const numeracion = await Numeracion.findByPk(numeracionId, { transaction: t });
+    const numeracion = await Numeracion.findByPk(numeracionId, {
+      transaction: t
+    });
     numeracion.ultimoNumero += 1;
-    const nroComprobante = `${numeracion.serie}-${numeracion.ultimoNumero.toString().padStart(7, "0")}`;
-console.log(importeIva10)
+    const nroComprobante = `${numeracion.serie}-${numeracion.ultimoNumero
+      .toString()
+      .padStart(7, "0")}`;
+    console.log(importeIva10);
     // Guardar venta
-    const venta = await Venta.create({
-      empresaId,
-      sucursalId,
-      listaPrecioId,
-      formaVentaId,
-      clienteId,
-      anulado: false,
-      enviado: false,
-      usuarioCreacionId: id,
-      fechaVenta,
-      fechaInicio: numeracion.inicioTimbrado,
-      fechaFin: numeracion.finTimbrado,
-      timbrado: numeracion.timbrado,
-      modoEntrega: 'CONTRAENTREGA',
-      nroComprobante,
-      porcDescuento,
-      importeIva5,
-      importeIva10,
-      importeIvaExenta,
-      importeDescuento,
-      importeNeto,
-      importeSubtotal,
-      importeTotal,
-      totalKg
-    }, { transaction: t });
+    const venta = await Venta.create(
+      {
+        empresaId,
+        sucursalId,
+        listaPrecioId,
+        formaVentaId,
+        clienteId,
+        anulado: false,
+        enviado: false,
+        usuarioCreacionId: id,
+        fechaVenta,
+        fechaInicio: numeracion.inicioTimbrado,
+        fechaFin: numeracion.finTimbrado,
+        timbrado: numeracion.timbrado,
+        modoEntrega: "CONTRAENTREGA",
+        nroComprobante,
+        porcDescuento,
+        importeIva5,
+        importeIva10,
+        importeIvaExenta,
+        importeDescuento,
+        importeNeto,
+        importeSubtotal,
+        importeTotal,
+        totalKg
+      },
+      { transaction: t }
+    );
 
     // Guardar detalles
     await VentaDetalle.bulkCreate(
       detalles.map(detalle => ({
         ventaId: venta.id,
-        ...detalle,
+        ...detalle
       })),
       { transaction: t }
     );
@@ -118,21 +125,21 @@ const anularVenta = async (req, res) => {
       await venta.update({
         anulado: true,
         fechaAnulacion: new Date(),
-        usuarioAnulacionId: req.empresaId, // Asegúrate de tener el usuario actual disponible en req
+        usuarioAnulacionId: req.empresaId // Asegúrate de tener el usuario actual disponible en req
       });
-      res.status(200).json({ message: 'Venta anulada exitosamente' });
+      res.status(200).json({ message: "Venta anulada exitosamente" });
     } else {
-      res.status(404).json({ error: 'Venta no encontrada' });
+      res.status(404).json({ error: "Venta no encontrada" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error al anular la venta' });
+    res.status(500).json({ error: "Error al anular la venta" });
   }
 };
 
 // Listar ventas paginadas y filtradas
 const listarVentas = async (req, res) => {
-  console.log('listarVentas')
+  console.log("listarVentas");
   try {
     const {
       page = 1,
@@ -146,25 +153,28 @@ const listarVentas = async (req, res) => {
       nroComprobante
     } = req.params;
     const { empresaId } = req.usuario;
-console.log(  page  ,
-  pageSize,
-  fechaDesde,
-  fechaHasta,
-  clienteId,
-  sucursalId,
-  listaPrecioId,
-  formaVentaId,
-  nroComprobante)
+    console.log(
+      page,
+      pageSize,
+      fechaDesde,
+      fechaHasta,
+      clienteId,
+      sucursalId,
+      listaPrecioId,
+      formaVentaId,
+      nroComprobante
+    );
     const condiciones = {
-      empresaId 
+      empresaId
     };
-
- /*   if (fechaDesde && fechaHasta) {
+    const desde = moment(fechaDesde).format("YYYY-MM-DD");
+    const hasta = moment(fechaHasta).format("YYYY-MM-DD");
+    console.log(fechaDesde);
+    console.log(desde);
+    if (desde && hasta) {
       condiciones.fechaVenta = {
-        [Op.and]: [
-          { [Op.lte]: new Date(fechaDesde) },
-          { [Op.gte]: new Date(fechaHasta) },
-        ]
+        [Op.lte]: desde,
+        [Op.gte]: hasta
       };
     }
 
@@ -182,13 +192,12 @@ console.log(  page  ,
 
     if (formaVentaId > 0) {
       condiciones.formaVentaId = formaVentaId;
-    }*/
+    }
     if (nroComprobante && nroComprobante.length > 2) {
       condiciones.nroComprobante = {
         [Op.iLike]: `%${nroComprobante.toLowerCase()}%`
       };
     }
-
 
     const offset = (page - 1) * pageSize;
     const { rows: ventas, count } = await Venta.findAndCountAll({
@@ -215,28 +224,27 @@ console.log(  page  ,
           as: "sucursal",
           attributes: ["descripcion", "direccion", "telefono", "cel"]
         }
-       
       ],
       offset,
-      limit: pageSize,
-    },);
-   
+      limit: pageSize
+    });
+
     res.status(200).json({
       total: count,
       totalPages: Math.ceil(count / pageSize),
       page: Number(page),
       pageSize: Number(pageSize),
-      ventas,
+      ventas
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error al listar las ventas' });
+    res.status(500).json({ error: "Error al listar las ventas" });
   }
 };
-  
+
 module.exports = {
   getById,
   createVenta,
   anularVenta,
-  listarVentas,
+  listarVentas
 };
