1965adcf987d846093956fae03db6bcf5c163c2a
Miguel Montania <mikemontania@gmail.com>
2024-07-01 15:04:40 -0400

    ajustes a4

--- src/helpers/pdfGenerator.js
+++ src/helpers/pdfGenerator.js
@@ -10,7 +10,7 @@ const {
   generarIva,
   generarCopia,
   dividirHoja
-} = require("./pdf-helpers.js");
+} = require("./pdf-helpers-a4.js");
 
 // Puedes establecer 'portrait' (vertical) o 'landscape' (horizontal)
 const propiedades = [
@@ -19,28 +19,49 @@ const propiedades = [
   { hoja: "LEGAL", size: [612, 1008], orientacion: "portrait" }
 ];
 const createInvoice = (cabecera, detalles) => {
+  const modo =  propiedades[1];
   const doc = new PDFDocument({
     margin: 0,
-    size: propiedades[2].size,
-    layout: propiedades[2].orientacion
+    size: modo.size,
+    layout: modo.orientacion
   });
-  /********copia 1 */
-  generarCabecera(doc, cabecera, 30);
-  generarDatosCliente(doc, cabecera, 120);
-  generarDetalles(doc, detalles, 165);
-  generarAhorro(doc, cabecera.importeDescuento, 435);
-  generarTotalTexto(doc, cabecera.importeTotal, 450);
-  generarIva(doc, cabecera, 465);
-  generarCopia(doc, "Original:Cliente", 480);
-  dividirHoja(doc);
-  /*******copia 2 */
-  generarCabecera(doc, cabecera, 520);
-  generarDatosCliente(doc, cabecera, 610);
-  generarDetalles(doc, detalles, 655);
-  generarAhorro(doc, cabecera.importeDescuento, 925);
-  generarTotalTexto(doc, cabecera.importeTotal, 940);
-  generarIva(doc, cabecera, 955);
-  generarCopia(doc, "Duplicado: Archivo", 970);
+  if (modo.hoja === "LEGAL") {
+     
+     generarCabecera(doc, cabecera, 30);
+    generarDatosCliente(doc, cabecera, 120);
+    generarDetalles(doc, detalles, 165);
+    generarAhorro(doc, cabecera.importeDescuento, 435);
+    generarTotalTexto(doc, cabecera.importeTotal, 450);
+    generarIva(doc, cabecera, 465);
+    generarCopia(doc, "Original", 480);
+    dividirHoja(doc);
+ 
+    generarCabecera(doc, cabecera, 520);
+    generarDatosCliente(doc, cabecera, 610);
+    generarDetalles(doc, detalles, 655);
+    generarAhorro(doc, cabecera.importeDescuento, 925);
+    generarTotalTexto(doc, cabecera.importeTotal, 940);
+    generarIva(doc, cabecera, 955);
+    generarCopia(doc, "Duplicado: Archivo", 970); 
+  
+  } else if (modo.hoja === "A4") {
+    generarCabecera(doc, cabecera, 10);
+    generarDatosCliente(doc, cabecera, 100);
+    generarDetalles(doc, detalles, 145);
+    generarAhorro(doc, cabecera.importeDescuento, 355);
+    generarTotalTexto(doc, cabecera.importeTotal, 370);
+    generarIva(doc, cabecera, 385);
+    generarCopia(doc, "Original", 400);
+    dividirHoja(doc);
+ 
+    generarCabecera(doc, cabecera, 430);
+    generarDatosCliente(doc, cabecera, 520);
+    generarDetalles(doc, detalles, 565);
+    generarAhorro(doc, cabecera.importeDescuento, 775);
+    generarTotalTexto(doc, cabecera.importeTotal, 790);
+    generarIva(doc, cabecera, 805);
+    generarCopia(doc, "Duplicado: Archivo", 820); 
+  }
   doc.end();
   return doc;
 };
